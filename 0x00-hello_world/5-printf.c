26b4b64884f452d27ef20b6cb0d88f86128038eb
Justice Obasi <ocjustc@gmail.com>
2022-12-11 15:30:28 -0800

    show data sizes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,13 @@
 
 /**
  * Print with printf
- * who knows the meaning 
+ * who knows the meaning
  * of printf
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	
 	return (0);
 
 
